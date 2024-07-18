01e3a2cc1309df4f7bd5e55a8ac62305022b9724
Juan Rodríguez <juansero29@gmail.com>
2024-07-18 08:28:53 +0200

    Trying to fix tree view

--- server.js
+++ server.js
@@ -9,22 +9,35 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.static('public'));
 
+// Recursive function to get .ged files
+function getGedFiles(dir, fileList = []) {
+    const files = fs.readdirSync(dir);
+    files.forEach(file => {
+        const filePath = path.join(dir, file);
+        const stat = fs.statSync(filePath);
+        if (stat.isDirectory()) {
+            getGedFiles(filePath, fileList);
+        } else if (file.endsWith('.ged')) {
+            fileList.push(filePath.replace(path.join(__dirname, 'public'), ''));
+        }
+    });
+    return fileList;
+}
+
 // Endpoint to list .ged files in the /ged directory
 app.get('/ged', (req, res) => {
     const gedDir = path.join(__dirname, 'public', 'ged');
-    fs.readdir(gedDir, (err, files) => {
-        if (err) {
-            return res.status(500).send('Unable to scan directory');
-        }
-        const gedFiles = files.filter(file => file.endsWith('.ged'));
+    try {
+        const gedFiles = getGedFiles(gedDir);
         res.json(gedFiles);
-    });
+    } catch (err) {
+        res.status(500).send('Unable to scan directory');
+    }
 });
 
 // Endpoint to serve individual .ged files
-app.get('/ged/:filename', (req, res) => {
-    const filename = req.params.filename;
-    const filePath = path.join(__dirname, 'public', 'ged', filename);
+app.get('/ged/*', (req, res) => {
+    const filePath = path.join(__dirname, 'public', req.params[0]);
     res.sendFile(filePath);
 });
 
