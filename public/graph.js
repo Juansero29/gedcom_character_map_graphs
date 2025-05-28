282b0a2d82d95d36d63822740ebfeddddcb54380
Juan Rodríguez <Juansero29@users.noreply.github.com>
2025-05-29 00:36:04 +0200

    Making mobile layout really work as intended
    Close button is visible and everything is scrollable in all different
    layout positions wether it is portrait or landscape

--- public/graph.js
+++ public/graph.js
@@ -675,8 +675,8 @@ function showModalContent(nodeA, nodeB = null, relationInfo = null) {
   document.body.style.overflow = "hidden";
 
   const isMobile = window.innerWidth < 768;
-  modal.style.width = "100vw";
-  modal.style.height = "100vh";
+	modal.classList.remove("hidden");
+  document.body.style.overflow = "hidden";
 
   // const svg = document.querySelector("svg");
   // svg.style.width = isMobile ? "100vw" : "50vw";
@@ -709,7 +709,9 @@ function showModalContent(nodeA, nodeB = null, relationInfo = null) {
 
 function closeModal() {
   document.getElementById("modalContainer").classList.add("hidden");
-  document.body.style.overflow = "auto";
+ 
+	document.body.style.overflow = "hidden";
+
   const svg = document.querySelector("svg");
   // svg.style.width = "100%";
   // svg.style.height = "100%";
@@ -744,8 +746,9 @@ function showModalContentForLink(
   const content = document.getElementById("modalContent");
   modal.classList.remove("hidden");
 
-  modal.style.width = "100vw";
-  modal.style.height = "100vh";
+	
+	modal.classList.remove("hidden");
+document.body.style.overflow = "hidden";
 
   const section = (person, relation) => `
     <div style="flex: 1; padding: 1em; border-right: 1px solid #ccc;">
